482609da5a7670c6fd59752f4d3ec1464f9ea53e
Fabien <fabien.sens@free.fr>
2023-03-16 16:07:21 +0100

    pire tuto

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,14 +2,49 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import {
+	CompanyContainer,
+	FooterWrapper,
+	LinkColumn,
+	LinkItem,
+	LinkList,
+	LinkTitle,
+	Slogan,
+	SocialContainer,
+	SocialIconsContainer,
+} from './FooterStyles';
 
 const Footer = () => {
-  return (
-    <div>
-      Footer
-    </div>
-  );
+	return (
+		<FooterWrapper>
+			<LinkList>
+				<LinkColumn>
+					<LinkTitle>Call</LinkTitle>
+					<LinkItem href="06060606">0606060606</LinkItem>
+				</LinkColumn>
+				<LinkColumn>
+					<LinkTitle>Email</LinkTitle>
+					<LinkItem href="toto@tata.titi">toto@tata.titi</LinkItem>
+				</LinkColumn>
+			</LinkList>
+			<SocialIconsContainer>
+				<CompanyContainer>
+					<Slogan>Des trucs incroyables</Slogan>
+				</CompanyContainer>
+				<SocialContainer>
+					<SocialIcons href="https://github.com">
+						<AiFillGithub size="3rem" />
+					</SocialIcons>
+					<SocialIcons href="https://linkedin.com">
+						<AiFillLinkedin size="3rem" />
+					</SocialIcons>
+					<SocialIcons href="https://instagram.com">
+						<AiFillInstagram size="3rem" />
+					</SocialIcons>
+				</SocialContainer>
+			</SocialIconsContainer>
+		</FooterWrapper>
+	);
 };
 
 export default Footer;
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import {
+	Section,
+	SectionDivider,
+	SectionText,
+	SectionTitle,
+} from '../../styles/GlobalComponents';
+import {
+	List,
+	ListContainer,
+	ListItem,
+	ListParagraph,
+	ListTitle,
+} from './TechnologiesStyles';
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+	<Section id="tech">
+		<SectionDivider />
+		<SectionTitle>Technologies</SectionTitle>
+		<SectionText>Pire tuto pour le coup celui-ci</SectionText>
+		<List>
+			<ListItem>
+				<DiReact size="3rem" />
+				<ListContainer>
+					<ListTitle>Front-End</ListTitle>
+					<ListParagraph>
+						Experience with <br />
+						React.js
+					</ListParagraph>
+				</ListContainer>
+			</ListItem>
+			<ListItem>
+				<DiFirebase size="3rem" />
+				<ListContainer>
+					<ListTitle>Back-End</ListTitle>
+					<ListParagraph>
+						Experience with <br />
+						Node.js
+					</ListParagraph>
+				</ListContainer>
+			</ListItem>
+			<ListItem>
+				<DiZend size="3rem" />
+				<ListContainer>
+					<ListTitle>LoL</ListTitle>
+					<ListParagraph>
+						Experience with <br />
+						Irelia mais bon
+					</ListParagraph>
+				</ListContainer>
+			</ListItem>
+		</List>
+	</Section>
 );
 
 export default Technologies;
